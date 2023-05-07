45dd368b1a6c50058e75fea3bb79a0f8f1d8eca9
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2023-05-07 12:09:33 -0500

    Add IO tests

---
+++ src/jvmTest/kotlin/IOTests.kt
@@ -0,0 +1,12 @@
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class IOTests {
+
+    @Test
+    fun `africa has 54 countries`() {
+        val countries = readCountries()
+        assertEquals(54, countries.size)
+    }
+    
+}
