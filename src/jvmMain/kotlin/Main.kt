27e79801ed70bfe0357c36c229c843505a4c9cd3
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2023-05-06 22:50:38 -0500

    Center app window

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,10 +1,9 @@
 @file:JvmName("AfricaApp")
 
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.window.Tray
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.ui.window.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -18,7 +17,8 @@ fun main() = application {
     }
     val icon = painterResource("africa.svg")
     Tray(icon)
-    Window(onCloseRequest = ::exitApplication, title = "Africa", icon = icon) {
+    val state = rememberWindowState(position = WindowPosition(Alignment.Center))
+    Window(onCloseRequest = ::exitApplication, title = "Africa", icon = icon, state = state) {
         App(africa)
     }
 }
