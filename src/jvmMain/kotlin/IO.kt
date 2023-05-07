f2ec1d97f5e9e73090b4ab1a1a250fa3b386fe53
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2023-05-07 12:38:15 -0500

    Read countries as stream from jar

--- src/jvmMain/kotlin/IO.kt
+++ src/jvmMain/kotlin/IO.kt
@@ -1,17 +1,15 @@
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
-import java.io.File
 
 private const val COUNTRIES_FILENAME = "africa.json"
 
-internal fun readCountries(filename: String = COUNTRIES_FILENAME): List<Country> {
-    val url = Africa::class.java.classLoader.getResource(filename)
-    val file = File(url.toURI())
-    val json = file.readText()
-    val serializer: KSerializer<Map<ISOCode, Country>> = serializer()
-    return Json.decodeFromString(serializer, json)
-        .map { entry ->
-            entry.value.also { it.code = entry.key }
-        }
-}
+internal fun readCountries(filename: String = COUNTRIES_FILENAME): List<Country> =
+    Africa::class.java.classLoader.getResourceAsStream(filename).use { stream ->
+        val json = stream.bufferedReader().readText()
+        val serializer: KSerializer<Map<ISOCode, Country>> = serializer()
+        Json.decodeFromString(serializer, json)
+            .map { entry ->
+                entry.value.also { it.code = entry.key }
+            }
+    }
