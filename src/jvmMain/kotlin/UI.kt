9a65ca56177422b88b384d73505b85b84451b430
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2023-05-06 22:31:55 -0500

    Add scroll bars

--- src/jvmMain/kotlin/UI.kt
+++ src/jvmMain/kotlin/UI.kt
@@ -1,6 +1,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,17 +18,26 @@ import androidx.compose.ui.unit.sp
 @Preview
 fun App(africa: Africa) {
     MaterialTheme {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center
-        ) {
-            val iterator = africa.iterator()
-            while (iterator.hasNext()) {
-                val entry = iterator.next()
-                item(key = entry.code) {
-                    CountryRow(country = entry.country)
+        Box(modifier = Modifier.fillMaxSize().padding(horizontal = 12.dp)) {
+            val state = rememberLazyListState()
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                state = state
+            ) {
+                val iterator = africa.iterator()
+                while (iterator.hasNext()) {
+                    val entry = iterator.next()
+                    item(key = entry.code) {
+                        CountryRow(country = entry.country)
+                    }
                 }
             }
+
+            VerticalScrollbar(
+                modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                adapter = rememberScrollbarAdapter(scrollState = state)
+            )
         }
     }
 }
