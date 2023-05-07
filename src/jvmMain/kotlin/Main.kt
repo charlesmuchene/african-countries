2774223880e73e7474c13723d7ad9859176af5f7
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2023-05-06 22:48:05 -0500

    Add app icon

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,6 +1,8 @@
 @file:JvmName("AfricaApp")
 
 import androidx.compose.runtime.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import kotlinx.coroutines.Dispatchers
@@ -14,8 +16,9 @@ fun main() = application {
             africa = Africa(readCountries())
         }
     }
-
-    Window(onCloseRequest = ::exitApplication, title = "Africa") {
+    val icon = painterResource("africa.svg")
+    Tray(icon)
+    Window(onCloseRequest = ::exitApplication, title = "Africa", icon = icon) {
         App(africa)
     }
 }
